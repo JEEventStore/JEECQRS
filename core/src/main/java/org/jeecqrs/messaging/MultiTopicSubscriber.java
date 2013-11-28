24f466ab73c7a15241ad8705ab9c07f440fbd802
Alexander Langer <alexander.langer@redrainbow.de>
2013-11-28 13:40:00 +0100

    Add MultiTopicSubscriber for pub-sub system

---
+++ core/src/main/java/org/jeecqrs/messaging/MultiTopicSubscriber.java
@@ -0,0 +1,39 @@
+package org.jeecqrs.messaging;
+
+import java.util.Set;
+
+/**
+ * Publish-subscribe messaging system subscriber supporting multiple topics
+ * of interest.
+ */
+public interface MultiTopicSubscriber<M> {
+
+    /**
+     * Receives a new message in the given topic.
+     * Called upon arrival of a new topic message.
+     * Must be thread-safe.
+     * 
+     * @param topic    the topic the message was published in
+     * @param message  the message
+     */
+    void receiveMessage(String topic, M message);
+
+    /**
+     * The publisher has canceled the subscription of this subscriber.
+     * Under circumstances the publisher might want to force a detach
+     * of a subscriber, for example when the number of retries to deliver a
+     * message has been exhausted.  This method is called after the
+     * publisher has canceled the subscription and may be used by the client
+     * to handle the situation gracefully.
+     */
+    void canceledSubscription();
+
+    /**
+     * Gets the set of topics this subscriber is interested in.
+     * Must not change between subscription and unsubscription.
+     * 
+     * @return  the topics of interest
+     */
+    Set<String> interestedInTopics();
+    
+}
