88da07d96cfba0bba886fa09fe1cb90992fa94c1
Alexander Langer <alexander.langer@redrainbow.de>
2013-12-01 20:41:26 +0100

    refactor package of AutoDiscoverCommandHandlerRegistry

---
+++ core/src/main/java/org/jeecqrs/commands/registry/autodiscover/AutoDiscoverCommandHandlerRegistry.java
@@ -0,0 +1,38 @@
+package org.jeecqrs.commands.registry.autodiscover;
+
+import java.util.Iterator;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import org.jeecqrs.commands.CommandHandler;
+import org.jeecqrs.commands.registry.AbstractCommandHandlerRegistry;
+
+/**
+ *
+ */
+public class AutoDiscoverCommandHandlerRegistry<C> extends AbstractCommandHandlerRegistry<C> {
+
+    private Logger log = Logger.getLogger(AutoDiscoverCommandHandlerRegistry.class.getName());
+
+    @Inject
+    private Instance<CommandHandler<C>> handlerInstances;
+
+    @PostConstruct
+    public void startup() {
+        log.info("Scanning command handlers");
+	Iterator<CommandHandler<C>> it = select(handlerInstances);
+        if (!it.hasNext())
+            log.warning("No CommandHandlers found");
+	while (it.hasNext()) {
+            CommandHandler h = it.next();
+            log.fine("Discovered CommandHandler: "+ h);
+            this.register(h.handledCommandType(), h);
+	}
+    }
+
+    protected Iterator<CommandHandler<C>> select(Instance<CommandHandler<C>> instances) {
+        return instances.iterator();
+    }
+    
+}
