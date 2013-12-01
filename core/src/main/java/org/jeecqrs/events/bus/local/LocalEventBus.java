2bc5737487ec374a15ed2373c5010713d40f9f67
Alexander Langer <alexander.langer@redrainbow.de>
2013-12-01 22:11:27 +0100

    Add LocalEventBus

---
+++ core/src/main/java/org/jeecqrs/events/bus/local/LocalEventBus.java
@@ -0,0 +1,111 @@
+package org.jeecqrs.events.bus.local;
+
+import org.jeecqrs.messaging.MultiTopicPublisher;
+import org.jeecqrs.messaging.MultiTopicSubscriber;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import org.jeecqrs.events.EventBus;
+import org.jeecqrs.events.EventBusInterest;
+import org.jeecqrs.events.EventBusListener;
+import org.jeecqrs.events.EventBusListenerRegistry;
+
+/**
+ *
+ */
+public class LocalEventBus<E> implements EventBus<E> {
+
+    private static final Logger log = Logger.getLogger(LocalEventBus.class.getCanonicalName());
+
+    @EJB(name="multiTopicPublisher")
+    private MultiTopicPublisher mtp;
+
+    @EJB(name="listenerRegistry")
+    private EventBusListenerRegistry<E> listenerRegistry;
+
+    @Resource(name="topicGenerationStrategy")
+    private Class<? extends TopicGenerationStrategy> topicGeneratorClass = DefaultTopicGenerationStrategy.class;
+    private TopicGenerationStrategy<E> topicGenerator;
+
+    private Map<EventBusListener, MultiTopicSubscriber<E>> map = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        if (topicGeneratorClass == null)
+            throw new IllegalArgumentException("topicGenerationStrategy must not be null");
+        try {
+            this.topicGenerator = topicGeneratorClass.newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            throw new RuntimeException("Cannot create topic generator instance: " + e.getMessage(), e);
+        }
+        subscribeListeners();
+    }
+
+    @Override
+    public void dispatch(E event) {
+	log.info("Dispatching event " + event.toString());
+        Class<? extends E> eventClass = (Class<? extends E>) event.getClass();
+        mtp.publish(topicGenerator.topicFor(eventClass), event);
+        mtp.publish(topicGenerator.wildcardTopic(), event);
+    }
+
+    protected void subscribeListeners() {
+        Set<EventBusListener<E>> listeners = listenerRegistry.allListeners();
+        for (EventBusListener<E> l : listeners)
+            subscribe(l);
+    }
+
+    protected void subscribe(EventBusListener<E> listener) {
+        MultiTopicSubscriber mts = subscriberFor(listener);
+        if (mts != null)
+            throw new IllegalStateException("Subscriber already subscribed: " + listener);
+        mts = createSubscriberFor(listener);
+        mtp.subscribe(mts);
+        map.put(listener, mts);
+	log.log(Level.INFO, "Now serving {0} listeners.", new Object[]{map.size()});
+    }
+
+    protected void unsubscribe(EventBusListener<E> listener) {
+        MultiTopicSubscriber mts = subscriberFor(listener);
+        if (mts == null)
+            throw new IllegalStateException("No such subscriber registered: " + listener);
+        map.remove(listener);
+        mtp.unsubscribe(mts);
+    }
+
+    private MultiTopicSubscriber<E> subscriberFor(EventBusListener<E> listener) {
+        return map.get(listener);
+    }
+
+    protected MultiTopicSubscriber<E> createSubscriberFor(final EventBusListener<E> listener) {
+        return new MultiTopicSubscriber<E>() {
+            @Override
+            public void receiveMessage(String topic, E message) {
+                listener.receiveEvent(message);
+            }
+
+            @Override
+            public void canceledSubscription() {
+                log.severe("Error: Forced unsubscription of EventBusListener " +
+                        listener + " / " + listener.getClass());
+                map.remove(listener);
+            }
+
+            @Override
+            public Set<String> interestedInTopics() {
+                EventBusInterest<E> interest = listener.interest();
+                Set<String> result = new HashSet<>();
+                for (Class<? extends E> c : interest.interestEventTypes())
+                    result.add(topicGenerator.topicFor(c));
+                return result;
+            }
+        };
+    }
+
+}
---
+++ core/src/main/java/org/jeecqrs/events/bus/local/TopicGenerationStrategy.java
@@ -0,0 +1,29 @@
+package org.jeecqrs.events.bus.local;
+
+import org.jeecqrs.messaging.MultiTopicPublisher;
+
+/**
+ * Provides the ability to generate a {@link MultiTopicPublisher> topic
+ * for a given event class.
+ * 
+ * @param <E>  the base event type
+ */
+public interface TopicGenerationStrategy<E> {
+
+    /**
+     * Gets the topic for the given event type.
+     * 
+     * @param eventType  the event type
+     * @return  the topic name
+     */
+    String topicFor(Class<? extends E> eventType);
+
+    /**
+     * Gets the name of the wildcard topic containing all events published
+     * on the event bus.
+     * 
+     * @return   the wildcard topic name
+     */
+    String wildcardTopic();
+    
+}
