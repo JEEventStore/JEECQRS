aeb99885b01fdfbac110a4f4e1dcd1c821632bd9
Alexander Langer <alexander.langer@redrainbow.de>
2014-05-12 14:47:03 +0200

    Add SagaTrackerEventBus interface with stricter quality guarantees

---
+++ core/src/main/java/org/jeecqrs/sagas/tracker/SagaTrackerEventBus.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013 Red Rainbow IT Solutions GmbH, Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.jeecqrs.sagas.tracker;
+
+import org.jeecqrs.event.EventBus;
+
+/**
+ * An EventBus with stricter quality guarantees.  Requires that events that have
+ * been accepted in a committed transaction are never lost, even on system
+ * crash or restart.
+ * The EventBus may use 2-Phase-Commits or use asynchronous delivery to
+ * avoid 2-PC.
+ */
+public interface SagaTrackerEventBus<E> extends EventBus<E> {
+
+}
