6d4752298832f6d302c6a4ba931ab6150e2cc9e3
Alexander Langer <alexander.langer@redrainbow.de>
2014-10-02 15:07:13 +0200

    remove unused import

--- core/src/main/java/org/jeecqrs/sagas/tracker/AbstractSagaTracker.java
+++ core/src/main/java/org/jeecqrs/sagas/tracker/AbstractSagaTracker.java
@@ -25,7 +25,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import org.jeecqrs.event.EventBus;
 import org.jeecqrs.sagas.SagaTracker;
 
 public abstract class AbstractSagaTracker<E> implements SagaTracker<E> {
